b351ead4b611bcfc79af77df476a3c23a7dd3720
Peterchen <peter1001136@gmail.com>
2020-11-30 00:57:25 +0800

    Initial Commit

---
+++ Bonus_Slider/Bonus_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bonus_SliderApp.swift
+//  Bonus_Slider
+//
+//  Created by 陳政沂 on 2020/11/30.
+//
+
+import SwiftUI
+
+@main
+struct Bonus_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bonus_Slider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bonus_Slider
+//
+//  Created by 陳政沂 on 2020/11/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
